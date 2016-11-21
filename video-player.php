6a5b78d77a02836b65c1d17432dab644cd546dc6
fedepena <fedepena@fedepena.local>
2016-11-21 17:23:37 +1300

    replaced tawk.to with drift in spanish pages

--- video-player.php
+++ video-player.php
@@ -56,21 +56,11 @@
           });
         }();
         drift.SNIPPET_VERSION = '0.3.1';
+        drift.config({
+          locale: "es"
+        });
         drift.load('33c7h6tzniik');
         </script>
         <!-- End of Async Drift Code -->
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/579be84d360c8e7a68c541b2/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-        </script>
-        <!--End of Tawk.to Script-->
     </body>
 </html>
